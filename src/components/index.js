79e26f6e80f4101fe721f9c94dc6728792d82268
Damian Soria <damian.soria@youbim.com>
2020-05-05 17:28:07 -0300

    feat: add new components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Card } from './Card';
+export { default as Chart } from './Chart';
+export { default as CountryPicker } from './CountryPicker';
+export { default as Layout } from './Layout';
+export { default as Logo } from './Logo';
